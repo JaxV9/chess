33ecc13c53346a1c8542c0c2af30d723d3ea8206
JaxV9 <layanj.pro@gmail.com>
2024-12-15 17:58:02 +0100

    chessboard created

---
+++ src/components/chessboard/chessboard.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useEffect, useState } from "react";
+import { Square } from "../square/square";
+
+
+export const Chessboard = () => {
+
+    const [square, setSquare] = useState<number[]>(
+        Array.from({ length: 64 }, (_, i) => i + 1)
+    );
+
+    return (
+        <>
+            <section className="chessboard-container">
+                <div className="chessboard-body">
+                    <div className="chessboard-content">
+                        {
+                            square.map((element, index) => (
+                                <Square key={index} elementProps={element}/>
+                            ))
+                        }
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
---
+++ src/components/square/square.tsx
@@ -0,0 +1,35 @@
+type SquarePropsType = {
+    elementProps: number,
+}
+
+export const Square = ({elementProps}: SquarePropsType) => {
+
+    const colorManager = (element: number) => {
+        let theme: string = '';
+        let currentLine: number = Number.isInteger(element / 8) ? element / 8 - 1 : Math.floor(element / 8);
+        let pairLine: boolean = currentLine % 2 == 0
+
+        if (pairLine && element % 2 !== 0) {
+            theme = "dark"
+        }
+        if (pairLine && element % 2 == 0) {
+            theme = "clear"
+        }
+        if (!pairLine && element % 2 !== 0) {
+            theme = "clear"
+        }
+        if (!pairLine && element % 2 == 0) {
+            theme = "dark"
+        }
+
+        return 'square ' + theme;
+    }
+
+    return (
+        <>
+            <div className={colorManager(elementProps)}>
+                {elementProps}
+            </div>
+        </>
+    )
+}
