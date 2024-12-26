e9d78765ebb1d61bca734b01f552704027dc0f50
JaxV9 <layanj.pro@gmail.com>
2024-12-26 18:28:50 +0100

    feat: preview-conflict

--- src/domain/chessboard/chessboard.ts
+++ src/domain/chessboard/chessboard.ts
@@ -2,13 +2,15 @@ export class ChessBoardDomain {
     
     constructor() {}
 
-    public static colorManager = (index: number, isPreviewed: boolean) => {
+    public static colorManager = (index: number, isPreviewed: boolean, isConflictPreview: boolean) => {
         let theme: string = '';
         const currentLine: number = Number.isInteger(index / 8) ? index / 8 - 1 : Math.floor(index / 8);
         const pairLine: boolean = currentLine % 2 == 0
 
-        if(isPreviewed){
+        if(isPreviewed && !isConflictPreview){
             return 'square preview'
+        } else if(isPreviewed && isConflictPreview){
+            return 'square preview-conflict'
         }
         if (pairLine && index % 2 !== 0) {
             theme = "clear"
