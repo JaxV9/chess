144ae5d4813046e7ed61940ee3af630ffe78df90
JaxV9 <layanj.pro@gmail.com>
2024-12-26 18:01:18 +0100

    feat: pawn preview added

--- src/domain/chessboard/chessboard.ts
+++ src/domain/chessboard/chessboard.ts
@@ -2,21 +2,24 @@ export class ChessBoardDomain {
     
     constructor() {}
 
-    public static colorManager = (element: number) => {
+    public static colorManager = (index: number, isPreviewed: boolean) => {
         let theme: string = '';
-        const currentLine: number = Number.isInteger(element / 8) ? element / 8 - 1 : Math.floor(element / 8);
+        const currentLine: number = Number.isInteger(index / 8) ? index / 8 - 1 : Math.floor(index / 8);
         const pairLine: boolean = currentLine % 2 == 0
 
-        if (pairLine && element % 2 !== 0) {
+        if(isPreviewed){
+            return 'square preview'
+        }
+        if (pairLine && index % 2 !== 0) {
             theme = "clear"
         }
-        if (pairLine && element % 2 == 0) {
+        if (pairLine && index % 2 == 0) {
             theme = "dark"
         }
-        if (!pairLine && element % 2 !== 0) {
+        if (!pairLine && index % 2 !== 0) {
             theme = "dark"
         }
-        if (!pairLine && element % 2 == 0) {
+        if (!pairLine && index % 2 == 0) {
             theme = "clear"
         }
         return 'square ' + theme;
