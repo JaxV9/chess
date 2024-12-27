99682c281bb0d9457521ecfb8a812d75178d2c76
JaxV9 <layanj.pro@gmail.com>
2024-12-27 16:00:28 +0100

    feat: invalid knight moves added

--- src/hooks/useMove.ts
+++ src/hooks/useMove.ts
@@ -1,37 +1,72 @@
+import { PieceRole } from "@/constants/constants";
 import { ChessPiece } from "@/data/chess";
-import { useEffect, useState } from "react";
+import { KnightDomain } from "@/domain/pieces/knight";
+import { PawnDomain } from "@/domain/pieces/pawn";
+import { useEffect, useMemo, useState } from "react";
 
 
 const useMove = () => {
+
+    const pawnDomain = useMemo(() => new PawnDomain(), []);
+    const knightDomain = useMemo(() => new KnightDomain(), []);
     
     const [currentPiece, setCurrentPiece] = useState<ChessPiece | null>(null)
     const [firstSquareTriggered, setFirstSquareTriggered] = useState<number | null>(null)
     const [secondSquareTriggered, setSecondSquareTriggered] = useState<number | null>(null)
     const [nextPos, setNextPos] = useState<number | null>(null)
     const [chessMod, setChessMod] = useState<ChessPiece | null>(null)
+    const [moveIsValid, setMoveIsValid] = useState<boolean | null>(null)
 
-    const move = (squareIndex: number, currentPiece: ChessPiece | null) => {
-        if(currentPiece){
-            setCurrentPiece(currentPiece)
-        }
-        if(!firstSquareTriggered){
+    //Manage the click to choose the currentpiece and his next pos
+    const move = (squareIndex: number, piece: ChessPiece | null) => {
+
+        if(!firstSquareTriggered && piece){
+            setCurrentPiece(piece)
             return setFirstSquareTriggered(squareIndex)
         }
-        if(firstSquareTriggered && !secondSquareTriggered){
+        if(firstSquareTriggered == secondSquareTriggered && !piece){
+            setSecondSquareTriggered(null)
+            return setSecondSquareTriggered(null)
+        }
+        if(firstSquareTriggered && !secondSquareTriggered && !piece){
             return setSecondSquareTriggered(squareIndex)
         }
     }
 
+    //Check if the initial pos and the next pos are clicked
     useEffect(() => {
-        if(secondSquareTriggered){
+        const egal = secondSquareTriggered == firstSquareTriggered
+        if(secondSquareTriggered && firstSquareTriggered && egal){
+            setFirstSquareTriggered(null)
+            setSecondSquareTriggered(null)
+            setCurrentPiece(null)
+        }
+        if(secondSquareTriggered && firstSquareTriggered && !egal){
             setNextPos(secondSquareTriggered)
             setFirstSquareTriggered(null)
             setSecondSquareTriggered(null)
         }
-    },[secondSquareTriggered])
+    },[secondSquareTriggered, firstSquareTriggered])
 
+    //Check if the player move is valid
     useEffect(() => {
         if(nextPos && currentPiece){
+            switch(currentPiece?.role){
+                case PieceRole.pawn_black:
+                case PieceRole.pawn_white:
+                    setMoveIsValid(pawnDomain.checkMove(nextPos, currentPiece))
+                case PieceRole.knight_black:
+                case PieceRole.knight_white:
+                    setMoveIsValid(knightDomain.checkMove(nextPos, currentPiece))
+            }
+            // console.log(nextPos)
+        }
+    },[nextPos, currentPiece, pawnDomain, knightDomain])
+
+
+    //Mod the chess position
+    useEffect(() => {
+        if(nextPos && currentPiece && moveIsValid === true){
             setChessMod({
                 id: currentPiece.id,
                 role: currentPiece.role,
@@ -40,8 +75,9 @@ const useMove = () => {
             })
             setNextPos(null)
             setCurrentPiece(null)
+            setMoveIsValid(null)
         }
-    },[nextPos, currentPiece])
+    },[nextPos, currentPiece, moveIsValid])
 
     return {
         move,
