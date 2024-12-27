56802471a7ad8e79789ef7080c4fdc1145388ad4
JaxV9 <layanj.pro@gmail.com>
2024-12-27 10:59:19 +0100

    feat: improve the move

--- src/components/chessboard/chessboard.tsx
+++ src/components/chessboard/chessboard.tsx
@@ -2,22 +2,40 @@
 import { data, ChessPiece } from "@/data/chess";
 import { Square } from "../square/square";
 import { ChessboardInfos } from "../ui/chessboardInfos/chessboardInfos";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ChessBoardDomain } from "@/domain/chessboard/chessboard";
 import useSquarePreview from "@/hooks/useSquarePreview";
+import useMove from "@/hooks/useMove";
 
 
 export const Chessboard = () => {
 
-    const { previewedSquare,  getAllPreviewedSquares, clearPreview } = useSquarePreview();
+    const { previewedSquare, getAllPreviewedSquares, clearPreview } = useSquarePreview();
+    const { move, chessMod, setChessMod } = useMove();
 
     const square = Array.from({ length: 64 }, (_, i) => i + 1);
     const [allPieces, setAllPieces] = useState<ChessPiece[]>(data);
 
-
     const colorManager = (index: number, isPreviewed: boolean, isConflictPreview: boolean) => {
         return ChessBoardDomain.colorManager(index, isPreviewed, isConflictPreview)
     }
+
+    const chessMove = (squareIndex: number, currentPiece: ChessPiece | null) => {
+        move(squareIndex, currentPiece)
+    }
+
+    useEffect(() => {
+        if (chessMod) {
+            setAllPieces(prev =>
+                prev.map(piece =>
+                    piece.id === chessMod.id ?
+                        { ...piece, pos: chessMod.pos }
+                        : piece
+                )
+            )
+            setChessMod(null)
+        }
+    }, [chessMod, setChessMod])
 
     return (
         <>
@@ -28,8 +46,8 @@ export const Chessboard = () => {
                             square.map((element, index) => (
                                 <Square key={index} indexProps={element} getAllPreviewedSquaresProps={getAllPreviewedSquares}
                                     previewedSquareProps={previewedSquare} clearPreviewProps={clearPreview}
-                                    colorManagerProps={colorManager}
-                                    allPiecesProps={allPieces} setAllPiecesProps={setAllPieces} />
+                                    colorManagerProps={colorManager} chessMoveProps={chessMove}
+                                    allPiecesProps={allPieces} />
                             ))
                         }
                     </div>
--- src/components/square/square.tsx
+++ src/components/square/square.tsx
@@ -1,44 +1,32 @@
 "use client";
+import { useEffect, useState } from "react";
 import { Piece } from "../piece/piece";
 import { ChessPiece } from "@/data/chess";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
-import { PawnDomain } from "@/domain/pieces/pawn";
 
 type SquarePropsType = {
     indexProps: number,
     allPiecesProps: ChessPiece[],
-    setAllPiecesProps: Dispatch<SetStateAction<ChessPiece[]>>,
     getAllPreviewedSquaresProps: (index: number, chessPiece: ChessPiece) => void,
     clearPreviewProps: () => void,
     previewedSquareProps: number[] | null,
-    colorManagerProps: (index: number, isPreviewed: boolean, isConflictPreview: boolean) => string
+    colorManagerProps: (index: number, isPreviewed: boolean, isConflictPreview: boolean) => string,
+    chessMoveProps: (squareIndex: number, currentPiece: ChessPiece | null) => void,
 }
 
-export const Square = ({ indexProps, allPiecesProps, setAllPiecesProps,
-    getAllPreviewedSquaresProps, clearPreviewProps, previewedSquareProps, colorManagerProps }: SquarePropsType) => {
-
-    const pawnDomain = new PawnDomain;
+export const Square = ({ indexProps, allPiecesProps,
+    getAllPreviewedSquaresProps, clearPreviewProps, previewedSquareProps, colorManagerProps,
+    chessMoveProps }: SquarePropsType) => {
 
     const [currentPiece, setCurrentPiece] = useState<ChessPiece | null>(null)
     const [isPreviewed, setIsPreviewed] = useState<boolean>(false)
     const [isConflictPreview, setIsConflictPreview] = useState<boolean>(false)
 
-    const moveForward = async () => {
-        if (!currentPiece) {
-            return null
+    const chessMove = async () => {
+        if(currentPiece){
+            chessMoveProps(indexProps, currentPiece)
+        }else {
+            chessMoveProps(indexProps, null)
         }
-        const nextPos = pawnDomain.forward(currentPiece.pos, currentPiece.color)
-        if (!nextPos) {
-            return null
-        }
-        clearPreviewProps()
-        setAllPiecesProps(prev =>
-            prev.map(piece =>
-                piece.id === currentPiece.id ?
-                    { ...piece, pos: nextPos }
-                    : piece
-            )
-        )
     }
 
     const preview = () => {
@@ -75,8 +63,8 @@ export const Square = ({ indexProps, allPiecesProps, setAllPiecesProps,
 
     return (
         <>
-            <div onMouseEnter={preview} onMouseLeave={clearPreviewProps}
-            onClick={moveForward} className={colorManagerProps(indexProps, isPreviewed, isConflictPreview)}>
+            <div onMouseEnter={preview} onMouseLeave={clearPreviewProps} onClick={chessMove}
+                className={colorManagerProps(indexProps, isPreviewed, isConflictPreview)}>
                 {currentPiece ?
                     <Piece currentPieceProps={currentPiece} />
                     : null
