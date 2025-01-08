939daab768fb1167a5c2a26692f499b5d0594a7c
JaxV9 <layanj.pro@gmail.com>
2025-01-08 15:44:11 +0100

    feat: hud added

---
+++ src/components/menu/menu.tsx
@@ -0,0 +1,17 @@
+
+
+export const Menu = () => {
+
+    return (
+        <>
+            <section className="menu-section">
+                <div className="menu-container">
+                    <div>
+                        <h2>New game</h2>
+                        <button className="menu-start-game-btn">Start</button>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
