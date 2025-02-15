30d317b9d3c613cfc617abc08ccfface6770b2dd
JaxV9 <layanj.pro@gmail.com>
2025-02-15 11:37:40 +0100

    feat: game engine store added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import StoreProvider from "./StoreProvider";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Chess game",
@@ -14,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={[inter.variable].join(" ")}>
         <StoreProvider>
           {children}
         </StoreProvider>
