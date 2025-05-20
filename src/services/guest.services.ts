8d00fd8e44741680be7472bd9616ec1bc417fe90
JaxV9 <layanj.pro@gmail.com>
2025-05-20 22:02:45 +0200

    update: http

--- src/services/guest.services.ts
+++ src/services/guest.services.ts
@@ -1,14 +1,10 @@
-import { Http } from "@/utils";
+import { Http, ResAction } from "@/utils";
 
 export class GuestServices {
   constructor(private http: Http) {}
 
-  async createGuest(): Promise<Response> {
-    try {
-      const response = await this.http.post("guest");
-      return await response.json();
-    } catch (error) {
-      throw await error;
-    }
+  async createGuest(): Promise<ResAction> {
+    const response = await this.http.post("guest");
+    return response
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,16 @@
+export type Success = "Success";
+
+export type Failure = "Failure";
+
+export type ResAction = {
+  payload: unknown;
+  status: Success | Failure;
+};
+
 export class Http {
   constructor(private baseUrl: string = "http://127.0.0.1:8000/") {}
 
-  async post(path: string, payload?: object | null): Promise<Response> {
+  async post(path: string, payload?: object | null): Promise<ResAction> {
     const response = await fetch(`${this.baseUrl + path}`, {
       method: "POST",
       credentials: 'include',
@@ -12,22 +21,37 @@ export class Http {
     });
 
     if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+      return {
+        payload: await response.json(),
+        status: "Failure",
+      };
     }
-    return response;
+    return {
+      payload: await response.json(),
+      status: "Success",
+    };
   }
 
-  async get(path: string): Promise<Response> {
+  async get(path: string): Promise<ResAction> {
     const response = await fetch(`${this.baseUrl + path}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
     });
-    return response;
+    if (!response.ok) {
+      return {
+        payload: await response.json(),
+        status: "Failure",
+      };
+    }
+    return {
+      payload: await response.json(),
+      status: "Success",
+    };
   }
 
-  async patch(path: string, payload: object | null): Promise<Response> {
+  async patch(path: string, payload: object | null): Promise<ResAction> {
     const response = await fetch(`${this.baseUrl + path}`, {
         method: "POST",
         headers: {
@@ -37,8 +61,14 @@ export class Http {
       });
   
       if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+        return {
+          payload: await response.json(),
+          status: "Failure",
+        };
       }
-      return response;
+      return {
+        payload: await response.json(),
+        status: "Success",
+      };
   }
 }
