98b8564bfb07222339d823f46db713f819f8f446
JaxV9 <layanj.pro@gmail.com>
2025-05-21 09:11:53 +0200

    feat: menu component

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -3,16 +3,32 @@
 import { guestUseCases } from "@/useCases/gateway.useCases";
 import { UiMenu } from "../ui/uiMenu/uiMenu";
 import { UiMenuBtn } from "../ui/uiMenuBtn/uiMenuBtn";
+import { useContext} from "react";
+import { PopUpContext } from "@/contexts/contextsProvider";
 
 export const Menu = () => {
+  const { setPopUpState } = useContext(PopUpContext);
+
   const createGuest = async () => {
-    return await guestUseCases.createGuest()
+    const response = await guestUseCases.createGuest();
+    if(response.status === 'Failure'){
+      setPopUpState(prev => ({
+        ...prev,
+        message: [...prev.message, "Failure"],
+        type: null,
+      }));
+    }
+    setPopUpState(prev => ({
+      ...prev,
+      message: [...prev.message, "Success"],
+      type: null,
+    }));
   };
 
   return (
     <>
-      <UiMenu>
-        <h2>Chess</h2>
+      <UiMenu title="Chess">
+        <UiMenuBtn callback={() => {}} text="Login" />
         <UiMenuBtn callback={createGuest} text="New game as guest" />
       </UiMenu>
     </>
